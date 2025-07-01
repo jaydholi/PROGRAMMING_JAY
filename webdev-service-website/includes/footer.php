760cdc4c3a3b87947479b922c7ffb767016d3548
Jay Dholi <jaydholi201020072@gmail.com>
2025-07-01 15:05:10 +0530

    commit

--- webdev-service-website/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- includes/footer.php -->
-<footer class="bg-dark text-white text-center p-3 mt-5">
-  <p>&copy; 2025 Jay Web Development Service | All Rights Reserved.</p>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/js/script.js"></script>
-</body>
-</html>
