411a8c65f3d4e0da5e54eef8b5d290034606aae2
Jay Dholi <jaydholi201020072@gmail.com>
2025-07-01 12:28:39 +0530

    commit

--- webdev-service-website/service.php
+++
@@ -1,6 +0,0 @@
-<?php include('includes/header.php'); ?>
-<div class="container mt-5">
-  <h2>Web Development Services</h2>
-  <p>We provide frontend, backend, e-commerce, and dynamic site solutions using HTML, CSS, JS, PHP, Bootstrap & more.</p>
-</div>
-<?php include('includes/footer.php'); ?>
